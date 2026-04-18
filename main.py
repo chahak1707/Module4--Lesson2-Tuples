ba0f8faa4b86443b0f2ccbfc93d28bcb3f6e5367
chahak1707 <pahujamanni1978@gmail.com>
2026-04-18 14:35:05 +0530

    Module4-lesson2

---
+++ main.py
@@ -0,0 +1,19 @@
+number_tuple= (10, 20, "Emma", "Kelly")
+print(number_tuple)
+
+string_tuple= ("Jessa", "Emma", "Kelly", 8, 9)
+print(string_tuple)
+print(len(string_tuple))
+print("length is",(len(string_tuple)))
+
+#indexing
+tuple1= ('P','Y','T','H','O','N')
+for i in range(4):
+    print(tuple1[i])
+
+string_tuple1= ("Jessa", "Emma", "Kelly", 8, 9)
+print(string_tuple1[4])
+print(string_tuple1[1:4])
+
+#concatenation
+print(tuple1+string_tuple1)
